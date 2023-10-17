bc3176b9a2e67a1deb035b02b2db0640fc95d929
Omar <omarsegura76300@gmail.com>
2023-10-17 16:22:18 +0200

    Calculatrice

---
+++ script.js
@@ -0,0 +1,35 @@
+let firstvalue = document.querySelector('#firstvalue');
+let operandes = document.querySelector('#operandes');
+let secondvalue = document.querySelector('#secondvalue');
+let result = document.querySelector('#result');
+let screen = firstvalue
+
+window.addEventListener('click', function (e) {
+    if (e.target.classList.contains('myvalue')) {
+        screen.value += e.target.value;
+    }
+    if (e.target.classList.contains('operandes')) {
+        operandes.value += e.target.value;
+
+        screen = secondvalue
+
+    }
+    if (e.target.classList.contains('result')) {
+       
+        if (operandes.value == '+') {
+            result.value = parseFloat(firstvalue.value) + parseFloat(secondvalue.value);
+        }
+        if (operandes.value == '-') {
+            result.value = parseFloat(firstvalue.value) - parseFloat(secondvalue.value);
+        }
+        if (operandes.value == '*') {
+            result.value = parseFloat(firstvalue.value) * parseFloat(secondvalue.value);
+        }
+        if (operandes.value == '/') {
+            result.value = parseFloat(firstvalue.value) / parseFloat(secondvalue.value);
+        }
+        
+    }
+ 
+});
+
